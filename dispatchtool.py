f8de7b62667a24c4d0ad9331ae9326c5dce0989c
Andrew Sturges <arsturges@lbl.gov>
2012-08-29 18:04:24 -0700

    fixed template inheritance, added dummy content

--- dispatchtool.py
+++ dispatchtool.py
@@ -3,12 +3,20 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index(color = "green"):
-    return render_template('layout.html', color=color)
+def index():
+    return render_template('home.html', title="Home")
+
+@app.route('/help')
+def help():
+    return render_template('help.html', title = "Help") 
 
 @app.route('/about')
 def about():
-    return 'Andy Satchwell, Nathan Addy, Andrew Sturges.'
+    return render_template('about.html', title = "About") 
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title = "Contact") 
 
 if __name__ == '__main__':
     app.run(debug=True) #disable this before pushing to production
