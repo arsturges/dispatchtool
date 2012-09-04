e503593458529975802e8f3b7b995a938481f3dc
Andrew Sturges <arsturges@lbl.gov>
2012-09-04 16:13:22 -0700

    added file upload

--- dispatchtool.py
+++ dispatchtool.py
@@ -1,10 +1,29 @@
-from flask import Flask
-from flask import render_template
-app = Flask(__name__)
+import os
+from flask import Flask, request, redirect, render_template, url_for, send_from_directory
+from werkzeug import secure_filename 
 
-@app.route('/')
+UPLOAD_FOLDER = 'user_uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'csv'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods = ['GET','POST'])
 def index():
-    return render_template('home.html', title="Home")
+	if request.method == 'POST':
+		file = request.files['file']
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return redirect(url_for('uploaded_file',filename=filename))
+	return render_template('home.html', title="Home")
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/help')
 def help():
