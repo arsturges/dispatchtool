06f14c7d6efcfa2e7739fa7bcca24df9e0a4e0d0
Andrew Sturges <andrew@ubuntu.(none)>
2012-10-18 17:22:30 -0700

    spacified. de-tabified. nothing else should have changed.

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ dispatchtool
 Andy Satchwell
 Nathan Addy
 Andrew Sturges
+
+Installation:
+$ git clone https://github.com/arsturges/dispatchtool.git
+$ git clone <path to Nathans attila server to clone dr_dispatch>
+$ sudo pip install flask
+$ cd dispatchtool
+$ python dispatchtool.py
--- dispatchtool.py
+++ dispatchtool.py
@@ -10,39 +10,39 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
-  return render_template('home.html', title = "Home") 
+    return render_template('home.html', title = "Home") 
 
 @app.route('/get_started', methods = ['GET','POST'])
 def get_started():
-	if request.method == 'POST':
-		print request.files['lmps'].filename
-		print request.files['dr'].filename
-		print request.files['load'].filename
-		file = request.files['lmps']
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			print filename
-			run_dr_dispatch(
-				"user_uploads/WECC_Common Case Reference DR.csv",
-				"user_uploads/WECC_Common Case LMPs_20120130.csv",
-				"user_uploads/WECC_Hourly Energy Load.csv",
-				"Inflexible")
-			return render_template('confirm_files.html',title="Confirm Files Submission")
-	else:
-		return render_template('get_started.html', title="Get Started")
+    if request.method == 'POST':
+        print request.files['lmps'].filename
+        print request.files['dr'].filename
+        print request.files['load'].filename
+        file = request.files['lmps']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print filename
+            run_dr_dispatch(
+                "user_uploads/WECC_Common Case Reference DR.csv",
+                "user_uploads/WECC_Common Case LMPs_20120130.csv",
+                "user_uploads/WECC_Hourly Energy Load.csv",
+                "Inflexible")
+            return render_template('confirm_files.html',title="Confirm Files Submission")
+    else:
+        return render_template('get_started.html', title="Get Started")
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/confirm_files')
-def confirm_files(lmps, dr, load): 
-	return render_template('confirm_files.html', title = "Confirm Files")
+def confirm_files(lmps, dr, load):
+    return render_template('confirm_files.html', title = "Confirm Files")
 
 @app.route('/help')
 def help():
